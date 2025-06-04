8479b9850ad6f2efebfee02f85ebea2e2101357e
Sidhant-1299 <sidhant.raj.khati@gmail.com>
2025-06-04 01:06:02 -0400

    Added MLR CPP package as project

--- src/components/Data/ProjectData.jsx
+++ src/components/Data/ProjectData.jsx
@@ -2,12 +2,20 @@ import classification_tiktok from "../../assets/Projects/classification_tiktok.p
 import jameswebb from "../../assets/Projects/james_webb.jpg";
 import kmeans from "../../assets/Projects/KMeans_Visualizer.jpg";
 import perceptron from "../../assets/Projects/perceptron.png";
+import mlr from "../../assets/Projects/MLR_regression.png";
 
 export const header = "Recent projects";
 
 export const MyProjects = [
   {
     id: 1,
+    url: "https://github.com/Sidhant-1299/mlr_cpp",
+    img: mlr,
+    header: "mlr_cpp",
+    description: "Fast C++ Multiple Linear Regression library with Python API",
+  },
+  {
+    id: 2,
     url: "https://github.com/Sidhant-1299/Exoplanet-Exploration",
     img: jameswebb,
     header: "Exoplanet Exploration EDA",
@@ -15,14 +23,14 @@ export const MyProjects = [
       "Analyze and understand the properties of discovered exoplanets to uncover patterns",
   },
   {
-    id: 2,
+    id: 3,
     url: "https://github.com/Sidhant-1299/Classification-with-ML",
     img: classification_tiktok,
     header: "Classification with ML",
     description: "Classifying videos using Random Forest",
   },
   {
-    id: 3,
+    id: 4,
     url: "https://github.com/Sidhant-1299/Kmeans_interactive_visualizer",
     img: kmeans,
     header: "Interactive Kmeans algorithm visualizer",
@@ -30,7 +38,7 @@ export const MyProjects = [
       "Using python to make an interactive interface for learning KMeans algorithm",
   },
   {
-    id: 4,
+    id: 5,
     url: "https://github.com/Sidhant-1299/Perceptron",
     img: perceptron,
     header: "Perceptron in Python",
