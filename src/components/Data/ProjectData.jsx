e6820f5b6c62fc87bd2bcd1c3f3ab260eda20967
Sidhant Raj Khati <63568908+Sidhant-1299@users.noreply.github.com>
2025-11-26 19:39:48 -0500

    Add new projects to MyProjects array
    Added two new projects: Semantic Book Recommender and Youtube Sentiment Analyzer, with their respective details and images.

--- src/components/Data/ProjectData.jsx
+++ src/components/Data/ProjectData.jsx
@@ -3,19 +3,36 @@ import jameswebb from "../../assets/Projects/james_webb.jpg";
 import kmeans from "../../assets/Projects/KMeans_Visualizer.jpg";
 import perceptron from "../../assets/Projects/perceptron.png";
 import mlr from "../../assets/Projects/MLR_regression.png";
+import book_recommender from "../../assets/Projects/semantic_book_recommender.png";
+import yt_analyzer from "../../assets/Projects/youtube_sentiment_analyzer.png";
+
 
 export const header = "Recent projects";
 
 export const MyProjects = [
   {
     id: 1,
+    url: "https://github.com/Sidhant-1299/Semantic-Book-Recommender",
+    img: book_recommender,
+    header: "Semantic Book Recommender",
+    description: "An interactive dashboard that recommends books based on meaning and mood",
+  },
+  {
+    id: 2,
+    url: "https://github.com/Sidhant-1299/Youtube-Sentiment-Analysis",
+    img: yt_analyzer,
+    header: "Youtube Sentiment Analyzer",
+    description: "A Chrome Extensionthat analyzes YouTube comments in real-time",
+  },
+  {
+    id: 3,
     url: "https://github.com/Sidhant-1299/mlr_cpp",
     img: mlr,
     header: "mlr_cpp",
     description: "Fast C++ Multiple Linear Regression library with Python API",
   },
   {
-    id: 2,
+    id: 4,
     url: "https://github.com/Sidhant-1299/Exoplanet-Exploration",
     img: jameswebb,
     header: "Exoplanet Exploration EDA",
@@ -23,14 +40,14 @@ export const MyProjects = [
       "Analyze and understand the properties of discovered exoplanets to uncover patterns",
   },
   {
-    id: 3,
+    id: 5,
     url: "https://github.com/Sidhant-1299/Classification-with-ML",
     img: classification_tiktok,
     header: "Classification with ML",
     description: "Classifying videos using Random Forest",
   },
   {
-    id: 4,
+    id: 6,
     url: "https://github.com/Sidhant-1299/Kmeans_interactive_visualizer",
     img: kmeans,
     header: "Interactive Kmeans algorithm visualizer",
@@ -38,7 +55,7 @@ export const MyProjects = [
       "Using python to make an interactive interface for learning KMeans algorithm",
   },
   {
-    id: 5,
+    id: 7,
     url: "https://github.com/Sidhant-1299/Perceptron",
     img: perceptron,
     header: "Perceptron in Python",
